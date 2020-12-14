706c5eb8486d919969c17a0a2920492000a79a00
hamlin.li <sclihuiming@163.com>
2020-12-14 14:37:40 +0800

    Fix a bug in the watch method that could throw events

--- src/lib/contract/method.js
+++ src/lib/contract/method.js
@@ -304,7 +304,7 @@ export default class Method {
                     since,
                     eventName: this.name,
                     sort: 'block_timestamp',
-                    blockNumber: 'latest',
+                    // blockNumber: 'latest',
                     filters: options.filters
                 }
                 if (options.resourceNode) {
@@ -336,8 +336,12 @@ export default class Method {
                     return event.block > lastBlock;
                 });
 
-                if (latestEvent)
+                if (latestEvent){
                     lastBlock = latestEvent.block;
+                    since = latestEvent.timestamp;
+                } else {
+                    since = since + 3000;
+                }
 
                 return newEvents;
             } catch (ex) {
@@ -361,7 +365,10 @@ export default class Method {
         bindListener();
 
         return {
-            start: bindListener,
+            start: () => {
+                since = Date.now() - 1000;
+                bindListener()
+            },
             stop: () => {
                 if (!listener)
                     return;
